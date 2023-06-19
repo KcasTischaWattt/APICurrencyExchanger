1445d33f74d767a00a8ff11e88b8e9eb104adf65
Matvey Tataurov <71022747+KcasTischaWattt@users.noreply.github.com>
2023-06-19 18:47:35 +0300

    Update Coollok4Application.java

--- coollok4/src/main/java/com/myroj/coollok4/Coollok4Application.java
+++ coollok4/src/main/java/com/myroj/coollok4/Coollok4Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Coollok4Application {
 
+	@Bean
+	public CurrencyRepository currencyRepository() {
+		return new CurrencyRepositoryImpl();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Coollok4Application.class, args);
 	}
